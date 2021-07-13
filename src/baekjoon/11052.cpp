52c6874699f372523d0f3d2c62370ecf1633c57b
Hyeri Jang <chjang5621@naver.com>
2021-07-13 21:28:07 +0900

    fixed untracked files

---
+++ src/baekjoon/11052.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+        int a;
+
+        printf("Hello, world!\n");
+    return 0;
+
+}
---
+++ src/baekjoon/io_practice.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+
+// 1000
+int main(int argc, const char **argv) {
+  int A, B;
+  scanf("%d %d", &A, &B);
+  printf("%d", A + B);
+}
+
+/*2557*/
+// int main(int argc, const char** argv) {
+
+//     printf("Hello World!");
+//     return 0;
+// }
+
+// using namespace std;
+
+// int main(void)
+// {
+//     string str;
+//     cin >> str;
+
+//     vector<string> dictionary;
+
+//     while (str.size())
+//         dictionary.push_back(str);
+//     str.erase(str.begin);
+// }
