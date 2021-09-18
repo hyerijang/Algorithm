ff986547141e129c5df0968f2f172c8d24b26b05
Hyeri Jang <chjang5621@naver.com>
2021-09-18 13:03:16 +0900

    기본 입출력

---
+++ src/python_algorithm_note/_파이썬/기본문법/입출력/기본 입출력 - 빠른 입력 .py
@@ -0,0 +1,9 @@
+# 빠르게 입력받아야 할때
+# sys.stdin.readline().rstrip() 사용 (1줄씩 입력)
+
+import sys
+
+text = sys.stdin.readline().rstrip()
+
+print("\n출력")
+print(text)
--- src/python_algorithm_note/_파이썬/기본문법/입출력/기본 입출력.py
+++ src/python_algorithm_note/_파이썬/기본문법/입출력/기본 입출력.py
@@ -5,20 +5,12 @@ n = int(input())
 # #리스트의 형태로 정수 데이터 입력받음
 # #공백 ' '을 기준으로 입력
 data = list(map(int, input().split()))
+# map 함수 : 리스트의 모든 원소에 각각 특정한 함수를 적용할 때 사용한다.
 
 data.sort(reverse=True)
 
 print(n)
 print(data)
-
-
-# 빠르게 입력받아야 할때
-# sys.stdin.readline().rstrip() 사용 (1줄씩 입력)
-
-text = sys.stdin.readline().rstrip()
-
-print("\n출력")
-print(text)
 
 
 n = 3
