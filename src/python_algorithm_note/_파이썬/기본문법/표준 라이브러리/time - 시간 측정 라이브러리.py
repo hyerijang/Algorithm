03759116a50b33c9c200c78647a7af015dee692c
Hyeri Jang <chjang5621@naver.com>
2021-09-18 17:49:44 +0900

    설명 추가

--- src/python_algorithm_note/_파이썬/기본문법/표준 라이브러리/time - 시간 측정 라이브러리.py
+++ src/python_algorithm_note/_파이썬/기본문법/표준 라이브러리/time - 시간 측정 라이브러리.py
@@ -6,4 +6,4 @@ start_time = time.time()
 end_time = time.time()
 # print(end_time)
 
-print('time :', end_time - start_time)
+print('time :', end_time - start_time)  # 정수부는 초단위, 소수부는 마이크로(micro) 초단위
