4e2ebd597cae6295f771d5769e8ba8a9cb5b1e71
Hyeri Jang <chjang5621@naver.com>
2021-09-24 19:10:55 +0900

    이진 탐색

--- src/python_algorithm_note/_파이썬/기본문법/입출력/기본 입출력 - 빠른 입력 .py
+++ src/python_algorithm_note/_파이썬/기본문법/입출력/기본 입출력 - 빠른 입력 .py
@@ -1,6 +1,10 @@
 # 빠르게 입력받아야 할때
 # sys.stdin.readline().rstrip() 사용 (1줄씩 입력)
 
+# readline() 사용시 오른쪽 끝에 줄바꿈 기호 삽입됨
+# .rstrip()? 가장 오른쪽의  줄바꿈 기호(엔터) 제거
+
+
 import sys
 
 text = sys.stdin.readline().rstrip()
