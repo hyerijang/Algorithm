260dc8fc6f4ad3a5fe89aafdd3c5760bf9b063d9
Hyeri Jang <chjang5621@naver.com>
2021-09-28 22:27:19 +0900

    자물쇠와 열쇠 예제 코드 추가

--- src/problem_solving/programmers/자물쇠와 열쇠.py
+++ src/problem_solving/programmers/자물쇠와 열쇠.py
@@ -1,84 +1,72 @@
 
 # https://programmers.co.kr/learn/courses/30/lessons/60059
-# FIXME: 실패. 50분 가량 구현했는데 잘 모르겠음
+#
+# ! 실패. 50분 가량 구현했는데 잘 모르겠음
 # TODO: 이코테 518쪽에 예제 코드 나와있음. 코드 보면서 복습할 것
 
-# def sumBumps(data):
-#     total = 0
-#     for line in data:
-#         total += sum(line)
-#     return total
+# [파이썬 예제 코드]
+# https://velog.io/@tjdud0123/%EC%9E%90%EB%AC%BC%EC%87%A0%EC%99%80-%EC%97%B4%EC%87%A0-2020-%EC%B9%B4%EC%B9%B4%EC%98%A4-%EA%B3%B5%EC%B1%84-python
+
+# [솔루션]
+# (1) M*2 + N 크기의 보드를 만들고 중앙에 좌물쇠를 배치한다.
+# (2) key를 4번 돌려가며 차례로 이동시킨다 (모든 경우의 수 시도)
+# (3) 중앙의 키가 모두 1이되면 unlock
+# def rotate90(arr): # 90도 돌리기
+# def attach(x, y, M, key, board): # 열쇠 넣어보기
+# def detach(x, y, M, key, board): # 열쇠 빼기
+# def check(board, M, N): # 모두 1인지 확인
+
+def attach(x, y, M, key, board):
+    for i in range(M):
+        for j in range(M):
+            board[x+i][y+j] += key[i][j]
 
 
-# def rotated(a):
-#     n = len(a)
-#     m = len(a[0])
-
-#     result = [[0] * n for _ in range(m)]
-
-#     for i in range(n):
-#         for j in range(m):
-#             result[j][n-i-1] = a[i][j]
-
-#     return result
+def detach(x, y, M, key, board):
+    for i in range(M):
+        for j in range(M):
+            board[x+i][y+j] -= key[i][j]
 
 
-# def ignEmpty(key):
-#     for line in key:
-#         if sum(line) == 0:
-#             key.remove(line)
-
-#     return key
+def rotate90(arr):
+    return list(zip(*arr[::-1]))
 
 
-# INF = 987654321
+def check(board, M, N):
+    for i in range(N):
+        for j in range(N):
+            if board[M+i][M+j] != 1:
+                return False
+    return True
 
 
-# def reverseZeroOne(a):
-#     n = len(a)
-#     m = len(a[0])
+def solution(key, lock):
+    M, N = len(key), len(lock)
 
-#     result = [[0] * n for _ in range(m)]
+    board = [[0] * (M*2 + N) for _ in range(M*2 + N)]
+    # 자물쇠 중앙 배치
+    for i in range(N):
+        for j in range(N):
+            board[M+i][M+j] = lock[i][j]
 
-#     for i in range(n):
-#         for j in range(m):
-#             if a[i][j] == 1:
-#                 a[i][j] = 0
-#             else:
-#                 a[i][j] = 1
+    rotated_key = key
+    # 모든 방향 (4번 루프)
+    for _ in range(4):
+        rotated_key = rotate90(rotated_key)
+        for x in range(1, M+N):
+            for y in range(1, M+N):
+                # 열쇠 넣어보기
+                attach(x, y, M, rotated_key, board)
+                # lock 가능 check
+                if(check(board, M, N)):
+                    return True
+                # 열쇠 빼기
+                detach(x, y, M, rotated_key, board)
 
-
-# def solution(key, lock):
-#     # 키에서 돌기가 없는 줄은 버리고 사용할 부분만 남긴다.
-#     key = ignEmpty(key)  # 상하의 빈 부분 무시
-#     key = ignEmpty(rotated(key))  # 90도 돌려서 다시 상하의 빈 부분 무시
-#     print(key)
-
-#     # 자물쇠에서 필요한 부분만 남긴다
-#     reverseZeroOne(lock)
-#     lock = ignEmpty(lock)  # 상하의 빈 부분 무시
-#     lock = ignEmpty(rotated(lock))  # 90도 돌려서 다시 상하의 빈 부분 무시
-#     # 키의 돌기 수가 부족한 경우 실패
-#     if sumBumps(key) < sumBumps(lock):
-#         return False
-#     print(lock)
-
-#     n = len(key)
-#     for l in lock:
-#         cnt = 0
-#         for k in key:
-#             if k not in l:
-#                 continue
-#             cnt += 1
-#             if cnt == n:
-#                 return True
-
-#     # 키를 90도씩 회전해가며 맞춰보기
-
-#     return True
+    return False
 
 
-# key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
-# lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
+lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
 
-# print(solution(key, lock))
+print(solution(key, lock))
