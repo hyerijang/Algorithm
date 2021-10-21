edf881076ee0b78ba27e43fea64519c7bfe56749
Hyeri Jang <chjang5621@naver.com>
2021-10-21 21:20:55 +0900

    211021

---
+++ src/problem_solving/programmers/다시 풀어볼 문제/불량사용자.py
@@ -0,0 +1,9 @@
+# https://programmers.co.kr/learn/courses/30/lessons/64064#
+# ! 실패 (2시간)
+# * Level 3
+# - product 으로 풀었는데.. 이렇게 풀면 테스트 5번에서 시간초과 발생함
+# - 다른 방법 고려해서 다시 풀어볼것! (쉬운 문제는 아님)
+
+# 시간초과나는 테스트 케이스
+# ["aaaaaaaa", "bbbbbbbb", "cccccccc", "dddddddd", "eeeeeeee", "ffffffff", "gggggggg", "hhhhhhhh"],
+# ["********", "********", "********", "********", "********", "********", "********"]
